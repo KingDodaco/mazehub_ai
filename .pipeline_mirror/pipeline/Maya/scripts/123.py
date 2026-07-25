a344bd276ca26014213d08f9ec28d9dbc4fa7f8c
Your GitHub Username <your-email@example.com>
2026-07-25 13:12:02 +0200

    fixed maya 123.py

--- .pipeline_mirror/pipeline/Maya/scripts/123.py
+++ .pipeline_mirror/pipeline/Maya/scripts/123.py
@@ -1,10 +1,5 @@
 import maya.cmds as cmds
 import os
-import sys
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
-if script_dir not in sys.path:
-    sys.path.insert(0, script_dir)
 
 import envPathResolver
 envPathResolver.setup()
