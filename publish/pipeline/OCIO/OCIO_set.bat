1a2c6dc2c3a06cc87e5a50e7bbc8abb42fe3713d
dodaco <116489352+KingDodaco@users.noreply.github.com>
2026-07-29 21:45:19 +0200

    removed publish from repo

--- publish/pipeline/OCIO/OCIO_set.bat
+++
@@ -1,9 +0,0 @@
-set ROOT=%~dp0
-
-set "OCIO=%ROOT%BU_nov2024_config.ocio"
-set "OCIO_ACTIVE_DISPLAYS=arri709 - Display:sRGB - Display"
-set "OCIO_ACTIVE_VIEWS=arri709 - View:Raw"
-
-::setx OCIO "%ROOT%BU_nov2024_config.ocio" & set "OCIO=%ROOT%BU_nov2024_config.ocio"
-::setx OCIO_ACTIVE_DISPLAYS "arri709 - Display:sRGB - Display" & set OCIO_ACTIVE_DISPLAYS="arri709 - Display:sRGB - Display"
-::setx OCIO_ACTIVE_VIEWS "arri709 - View:Raw" & set OCIO_ACTIVE_VIEWS="arri709 - View:Raw"
--- publish/pipeline/launch_mazehub.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-cd /d "%~dp0"
-if exist "MazeHub.exe" (
-    start "" "MazeHub.exe"
-    exit /b 0
-)
-echo MazeHub.exe not found in %~dp0
-exit /b 1
